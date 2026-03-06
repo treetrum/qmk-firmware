09690e5a5a492bdfec5fdae6056eafdc12f986b2
Sam Davis <sam@sjd.co>
2026-03-06 14:04:52 +1100

    add q2 overrides for treetrum keymap

---
+++ keyboards/keychron/q2/ansi_encoder/keymaps/treetrum/config.h
@@ -0,0 +1,5 @@
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
